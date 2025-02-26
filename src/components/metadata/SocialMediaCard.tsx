e93b154e065db276b52fc166a6665912fe41c60c
duy-nq <nqduy9207@gmail.com>
2025-02-26 14:35:11 +0700

    style: minor change in social media card

--- src/components/metadata/SocialMediaCard.tsx
+++ src/components/metadata/SocialMediaCard.tsx
@@ -104,10 +104,10 @@ export default function SocialMediaCard() {
         <div className="no-scrollbar relative w-full max-w-[700px] overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
           <div className="px-2 pr-14">
             <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
-              Edit Personal Information
+              Edit Company Information
             </h4>
             <p className="mb-6 text-sm text-gray-500 dark:text-gray-400 lg:mb-7">
-              Update your details to keep your profile up-to-date.
+              Update your details to keep your landing page up-to-date.
             </p>
           </div>
           <form className="flex flex-col">
@@ -128,7 +128,7 @@ export default function SocialMediaCard() {
               </div>
               <div className="mt-7">
                 <h5 className="mb-5 text-lg font-medium text-gray-800 dark:text-white/90 lg:mb-6">
-                  Personal Information
+                  {companyInformation.name} Logo
                 </h5>
 
                 <div className="grid grid-cols-1 gap-x-6 gap-y-5 lg:grid-cols-2">

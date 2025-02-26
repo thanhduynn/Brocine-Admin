f9119b0cda5d2dd84c7b67d20e39f5859dded705
duy-nq <nqduy9207@gmail.com>
2025-02-26 17:17:18 +0700

    feat: add new components and constants for controlling hero section

---
+++ src/components/common/ResponsiveView.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { DEFAULT_HERO_IMAGE } from "@/constants/images";
+import HeroSection from "@/types/hero.type";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+export default function ResponsiveView({
+  title,
+  subtitle,
+  imageUrl,
+}: HeroSection) {
+  const [image, setImage] = useState(imageUrl || DEFAULT_HERO_IMAGE);
+
+  useEffect(() => {
+    setImage(imageUrl || DEFAULT_HERO_IMAGE);
+  }, [imageUrl]);
+  
+  return (
+    <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
+      <h4 className="text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-6">
+        Preview
+      </h4>
+      <div className="relative">
+        <Image
+          src={image}
+          alt="Cover"
+          className="w-full border border-gray-200 rounded-xl dark:border-gray-800"
+          width={1054}
+          height={600}
+          onError={() => setImage(DEFAULT_HERO_IMAGE)}
+        />
+        {/* Overlay Container */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+          <h1 className="text-4xl font-bold text-white drop-shadow-lg">{title}</h1>
+          <h3 className="text-xl text-white drop-shadow-lg w-4/5">{subtitle}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/constants/images.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_HERO_IMAGE="/images/grid-image/image-01.png"
+export const DEFAULT_LOGO_IMAGE="/images/users/owner.png"

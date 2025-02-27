d0b4003eb5c726fa97f53a22f5e1030542a4b01b
quocduynn <quocduy@nung-ning.com>
2025-02-27 15:30:50 +0700

    feat: implement work-page UI with fake data

--- src/app/(admin)/(others-pages)/(page-content)/work/categories/page.tsx
+++ src/app/(admin)/(others-pages)/(page-content)/work/categories/page.tsx
@@ -1,4 +1,44 @@
-export default function WorkWorkDetail() {
+"use client"
+import CategoryCard from "@/components/work/CategoryCard";
+import { GLOBAL_WORK_TYPES } from "@/constants/types";
+import { useWorkStore } from "@/stores/work.store";
+import Tag from "@/types/tag.type";
+import { useEffect } from "react";
+
+export default function WorkCategories() {
+  const {setWorkStore} = useWorkStore();
+
+  const placeholder: Tag[] = [
+    {
+      id: "1",
+      tagName: GLOBAL_WORK_TYPES.brandingFilm,
+    },
+    {
+      id: "2",
+      tagName: GLOBAL_WORK_TYPES.mv,
+    },
+    {
+      id: "3",
+      tagName: GLOBAL_WORK_TYPES.docs,
+    },
+    {
+      id: "4",
+      tagName: GLOBAL_WORK_TYPES.talkshow,
+    },
+    {
+      id: "5",
+      tagName: GLOBAL_WORK_TYPES.tvc,
+    },
+    {
+      id: "6",
+      tagName: GLOBAL_WORK_TYPES.event,
+    },
+  ]
+
+  useEffect(() => {
+    setWorkStore('tagData', placeholder)
+  }, [])
+
   return (
     <div>
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
@@ -6,7 +46,7 @@ export default function WorkWorkDetail() {
           Work - Categories
         </h3>
         <div className="space-y-6">
-          
+          <CategoryCard />
         </div>
       </div>
     </div>
--- src/app/(admin)/(others-pages)/(page-content)/work/work-detail/page.tsx
+++ src/app/(admin)/(others-pages)/(page-content)/work/work-detail/page.tsx
@@ -1,4 +1,86 @@
-export default function WorkCategories() {
+"use client"
+import UsefulTable from "@/components/common/UsefulTable";
+import { useWorkStore } from "@/stores/work.store";
+import Project from "@/types/project.type.";
+import { useEffect } from "react";
+
+const projectData: Project[] = [
+  {
+    "id": "101",
+    "title": "Project Alpha",
+    "subtitle": "A journey through innovation",
+    "type": "Branding Film",
+    "videoUrl": "https://example.com/alpha.mp4",
+    "brand": "Tech Corp",
+    "productionCompany": "Vision Films",
+    "execusiveProducer": "Bob Smith",
+    "director": [
+      { "id": "1", "name": "Alice Johnson", "role": "Director", "imageUrl": "/images/user/user-10.jpg" }
+    ]
+  },
+  {
+    "id": "102",
+    "title": "Project Beta",
+    "subtitle": "Exploring the unknown",
+    "type": "Event Video",
+    "videoUrl": "https://example.com/beta.mp4",
+    "brand": "Future Industries",
+    "productionCompany": "Nova Studios",
+    "execusiveProducer": "Bob Smith",
+    "director": [
+      { "id": "3", "name": "Charlie Davis", "role": "Director", "imageUrl": "/images/user/user-07.jpg" }
+    ]
+  },
+  {
+    "id": "103",
+    "title": "Project Gamma",
+    "subtitle": "Breaking boundaries",
+    "type": "TVC",
+    "videoUrl": "https://example.com/gamma.mp4",
+    "brand": "NextGen",
+    "productionCompany": "Skyline Pictures",
+    "execusiveProducer": "Bob Smith",
+    "director": [
+      { "id": "4", "name": "Diana Lee", "role": "Director", "imageUrl": "/images/user/user-14.jpg" }
+    ]
+  },
+  {
+    "id": "104",
+    "title": "Project Delta",
+    "subtitle": "A vision for tomorrow",
+    "type": "TVC",
+    "videoUrl": "https://example.com/delta.mp4",
+    "brand": "InnovateX",
+    "productionCompany": "Cinematic Arts",
+    "execusiveProducer": "Bob Smith",
+    "director": [
+      { "id": "2", "name": "Bob Smith", "role": "Executive Producer", "imageUrl": "/images/user/user-01.jpg" },
+      { "id": "5", "name": "Ethan White", "role": "Director", "imageUrl": "/images/user/user-03.jpg" }
+    ]
+  },
+  {
+    "id": "105",
+    "title": "REALME C75 EVERYTHING PROOF",
+    "subtitle": "Built to endure with IP69 dust and water resistance. Stay powered with 6000mAh massive battery. Smart at your fingertip with advanced AI features.",
+    "type": "TVC",
+    "videoUrl": "https://example.com/epsilon.mp4",
+    "brand": "StoryWorks",
+    "productionCompany": "Luminary Studios",
+    "execusiveProducer": "Bob Smith",
+    "director": [
+      { "id": "1", "name": "Alice Johnson", "role": "Director", "imageUrl": "/images/user/user-02.jpg" },
+      { "id": "3", "name": "Charlie Davis", "role": "Director", "imageUrl": "/images/user/user-03.jpg" }
+    ]
+  }
+];
+
+export default function WorkWorkDetail() {
+  const { setWorkStore } = useWorkStore();
+  
+  useEffect(() => {
+    setWorkStore("projectData", projectData);
+  }, [])
+
   return (
     <div>
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
@@ -6,7 +88,7 @@ export default function WorkCategories() {
           Work - Work detail
         </h3>
         <div className="space-y-6">
-          
+          <UsefulTable />
         </div>
       </div>
     </div>
